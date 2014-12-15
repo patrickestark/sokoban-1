c298fb99d8322ecf838a4e3421ae843c0aead703
Taylor O'Brien <t08rien@gmail.com>
2014-12-14 22:15:15 -0600

    add a couple tests; tried to get private functions tested

--- spec/level_loader_spec.lua
+++ spec/level_loader_spec.lua
@@ -1,9 +1,36 @@
-Level = require 'src.level_loader'
+local Level = require 'src.level_loader'
 
-describe('Level.load', function()
-  it('opens the correct level', function()
-    Level.init('./assets/levels')
-    level = Level.load('level1')
-    assert.are.equal(level.title, 'Simplest Possible')
+describe('Level', function()
+  describe('_get_rows', function()
+    pending('need to figure out how to test privates')
+    --[=[
+    it('separates the rows from a layout', function()
+      local layout = [[
+      #####
+      #@ .#
+      #####
+      ]]
+      for row in Level._get_rows(layout) do
+        assert.are.equal(5, #row)
+      end
+    end)
+    ]=]
+  end)
+
+  describe('load', function()
+    setup(function()
+      Level.init('./assets/levels')
+    end)
+
+    it('opens the correct level', function()
+      level = Level.load('level1')
+      assert.are.equal('Simplest Possible', level.title)
+    end)
+
+    it("doesn't change the package.path outside of scope", function()
+      local prev_path = package.path
+      Level.load('level1')
+      assert.are.equal(prev_path, package.path)
+    end)
   end)
 end)
--- src/level_loader.lua
+++ src/level_loader.lua
@@ -1,26 +1,37 @@
 local Level = {}
 
-local level_dir
+local LEVELS_PATH
+
 local function level_loader(level_name)
   package_path = package.path
-  package.path = level_dir .. '?.lua;' .. package.path
+  package.path = LEVELS_PATH .. '?.lua;' .. package.path
   local level = require(level_name)
   package.path = package_path
   return level
 end
 
+local function get_rows(layout)
+  return string.gmatch(layout, '%S+%s?%S+')
+end
+
 Level.init = function(levels_path)
-  level_dir = levels_path
-  if string.sub(level_dir, -1) ~= '/' then
-    level_dir = level_dir .. '/'
+  LEVELS_PATH = levels_path
+  if string.sub(LEVELS_PATH, -1) ~= '/' then
+    LEVELS_PATH = LEVELS_PATH .. '/'
   end
 end
 
 Level.load = function(level_name)
   local level = level_loader(level_name)
-  for row in string.gmatch(level.layout, '%S+') do
+  for row in get_rows(level.layout) do
   end
   return level
+end
+
+-- Expose privates for testing
+if describe then
+  Level._level_loader = level_loader
+  Level._get_rows = get_rows
 end
 
 return Level
